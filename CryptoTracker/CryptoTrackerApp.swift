7e15fe466e9bda1caf624a670fe0a042c23ba9cd
Gokhnan Bozkurt <gkhnbzkrt1000@gmail.com>
2022-06-27 19:59:35 +0300

    Initial Commit

---
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by Gokhan Bozkurt on 27.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
